f46439ccd49e8d23cfb2ce8981f1a9ae20a99c56
testmkm0515 <testmkm0515@naver.com>
2026-07-22 17:50:10 +0900

    0722수업자료

--- JavaProject/src/conditioin/IfElse.java
+++ JavaProject/src/conditioin/IfElse.java
@@ -50,6 +50,22 @@ public class IfElse {
 		
 		System.out.println("성적 : " + grade);
 		
+		// 중첩 if : 특별한 기능은 아니고 코드 구조를 의미 
+		score = sc.nextInt();
+		String grade1;
+		if(score >= 90) {
+			if(score>=95)
+				grade1="A+";
+			else
+				grade1="A";
+			
+		}else {
+			if(score>=85)
+				grade1="B+";
+			else
+				grade1="B";			
+		}
+		
 		sc.close();
 	}
 
---
+++ JavaProject/src/loop/For.java
@@ -0,0 +1,33 @@
+package loop;
+
+public class For {
+
+	public static void main(String[] args) {
+		// for(초기식;조건식;증감식){반복문장}
+		int i; //main 메소드 내 어디에서든 사용 가능
+		for(i=1; i<=10; i++) {
+			System.out.println(i);
+		}
+		System.out.print("i 최종 값 : ");
+		System.out.println(i);
+		
+		for(int j=20; j<=30; j+=2) { //j는 for 문 내부의 지역변수
+			System.out.println(j);
+		}
+		//System.out.print("j 최종 값 : ");
+		//System.out.println(j);	
+		
+		//1~100까지 더한 값을 확인
+		//10+5+6+...+100
+		//1+2+3+
+		int k, sum=0;
+		for(k=1;k<=100;k++) {
+			// sum=0;
+			sum += k;
+		}
+		
+		System.out.println("1~"+(k-1)+" 합 : "+sum);
+
+	}
+
+}
