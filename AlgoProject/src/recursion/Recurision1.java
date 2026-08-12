a9e3f3641d8376ac8a34fa42699df51c74d8f87e
testmkm0515 <testmkm0515@naver.com>
2026-08-12 20:12:05 +0900

    0812 수업 commit

---
+++ AlgoProject/src/recursion/Recurision1.java
@@ -0,0 +1,23 @@
+package recursion;
+
+public class Recurision1 {
+	//SUM(n) = n+(n-1)+(n-2)+(n-3)+....+1
+	//SUM(5) = 5+SUM(4)
+	//SUM(4) = 4+SUM(3)
+	//SUM(3) = 3+SUM(2)
+	//SUM(2) = 2+SUM(1)
+	//SUM(1) = 1+SUM(0)
+	//SUM(0) : return 0
+	
+	public static void main(String[] args) {
+		System.out.println(sum(5));
+	}
+	static int sum(int n) {
+		if(n==0) {
+			return 0;			
+		}else {
+			return n+sum(n-1);
+		}
+	}
+
+}
