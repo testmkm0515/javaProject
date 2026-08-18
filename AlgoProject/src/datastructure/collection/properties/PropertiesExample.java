c3ebd026e3fba4991aeb24c67724f4e7206c51e1
testmkm0515 <testmkm0515@naver.com>
2026-08-18 18:17:29 +0900

    0818 수업자료

---
+++ AlgoProject/src/datastructure/collection/properties/PropertiesExample.java
@@ -0,0 +1,37 @@
+package datastructure.collection.properties;
+
+import java.io.FileReader;
+import java.net.URLDecoder;
+import java.util.Properties;
+
+public class PropertiesExample {
+
+	public static void main(String[] args) throws Exception {
+		// Properties 파일 읽어오기
+		Properties properties = new Properties();
+		//읽어오려는 property 파일의 경로 
+		String path = PropertiesExample.class.getResource("database.properties").getPath();
+		path = URLDecoder.decode(path,"utf-8");
+		properties.load(new FileReader(path));
+		
+		String driver = properties.getProperty("driver");
+		String url = properties.getProperty("url");
+		String username = properties.getProperty("username");
+		String password = properties.getProperty("password");
+		
+		System.out.println("driver : " + driver);
+		System.out.println("url : " + url);
+		System.out.println("username : " + username);
+		System.out.println("password : " + password);
+
+	}
+
+}
+
+
+
+
+
+
+
+
